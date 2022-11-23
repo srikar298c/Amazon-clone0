82ed12ec0694d92ff4ab97cbd796a963ec41942b
sri-gudakesa2703 <srikar2985969c@gmail.com>
2022-11-23 06:08:24 +0530

    updated a lot files and added new file

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyCl70USY3YDJ33vhbTPSkduw031aPyCsfM",
+  authDomain: "clone0-f3360.firebaseapp.com",
+  projectId: "clone0-f3360",
+  storageBucket: "clone0-f3360.appspot.com",
+  messagingSenderId: "494145888183",
+  appId: "1:494145888183:web:b476f3169f2d01b4e05232",
+  measurementId: "G-0B4JH3907B"
+};
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import Image from "next/image";
-
+import {signIn, signOut, useSession} from "next-auth/react"
+import {useRouter} from "next/router"
+import{
+  ShoppingCartIcon
+} from "@heroicons/react/24/outline"
 function Header() {
+  const {session} = useSession();
+  if(session){
+    return(<span>Loading</span>)
+  }
+ const router = useRouter();
+
   return (
     <header>
         <div className="flex items-center bg-amazon_blue p-1 flex-grow py-2">
           <h1 className="text-white text-xs">Sasta</h1>
             <div className=" mt-2 flex items-center flex-grow sm:flex-grow-0">
                 <Image
+                onClick={() => router.push("/")}
                 src ="https://links.papareact.com/f90"
                 width={150}
                 height ={40}
@@ -22,8 +33,8 @@ function Header() {
 </svg>
             </div>
             <div className="text-white flex iems-center text-xs space-x-6 mx-6 whitespace-nowrap">
-              <div className='link'>
-                <p>Hello srikar</p>
+              <div onClick={!session ? signIn: signOut} className='cusor-pointer link'>
+                {session ? `Hello, ${session.user.name}`:"Sign In"}
                 <p className='font-extrabold md:text-sm'>Account & Lists</p>
               </div>
             <div className='link' >
@@ -32,10 +43,10 @@ function Header() {
             </div>
             <span className='absolute top-0 right-0 md:right-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-bold'>0</span>
             <div className='realtive link flex items-center'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z" />
-</svg>
-
+</svg> */}
+<ShoppingCartIcon className=' h-8'/>
 <p className=' hidden md:inline font-extrabold md:text-sm mt-2'>Basket</p>
  </div>
  </div>
